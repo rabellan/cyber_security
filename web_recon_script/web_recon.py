63ddba2db25f3976b8de3681c6aac18b1e95217b
rabellan <roland.abellano@gmail.com>
2024-07-23 19:42:29 -0700

    added web recon script

---
+++ web_recon_script/web_recon.py
@@ -0,0 +1,25 @@
+from subprocess import call
+import hashlib
+import json
+import requests
+ 
+domain = 'YOUR-DOMAIN-HERE'
+ 
+uuid = hashlib.md5(domain.encode('utf-8')).hexdigest()
+ 
+result = requests.get('http://' + domain)
+ 
+ssl_result = requests.get('https://' + domain).status_code
+ 
+if(ssl_result == 200):
+  uses_ssl = True
+else:
+  uses_ssl = False
+ 
+uses_css = (result.text.find('<link rel="stylesheet"') > -1)
+ 
+uses_js = (result.text.find('<script language="JavaScript"') > -1)
+ 
+profile = { 'uuid': uuid, 'name': domain, 'uses_css': uses_css, 'uses_js': uses_js }
+ 
+print(json.dumps(profile))
